fcdcbc8ef9ecb798ace246471b6c573be5696034
Nyantemafy <antema.fy01@gmail.com>
2025-08-29 19:12:01 +0300

    initialisation dolibar et react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost/dolibarr/htdocs/api/index.php/products?limit=10", {
+      method: "GET",
+      headers: {
+        "DOLAPIKEY": "wf1WlH719z7B4VyNwLeY3k18WScWHog8", 
+        "Accept": "application/json"
+      }
+    })
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("Erreur API " + res.status);
+        }
+        return res.json();
+      })
+      .then(data => setProducts(data))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "20px" }}>
+      <h1>Liste des produits Dolibarr</h1>
+      <ul>
+        {products.map((p) => (
+          <li key={p.id}>
+            {p.ref} - {p.label} - {p.price} €
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
